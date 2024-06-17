b4e4858574395824c7a78dabf6085b6864aad512
dssantservicenow <dsantsnow@gmail.com>
2024-06-17 18:03:44 -0300

    Update novo.java
    Comentário adicionado #STRY0010030

--- novo.java
+++ novo.java
@@ -1,6 +1,8 @@
 // This is a simple Java program.
 // FileName : "HelloWorld.java".
 
+
+//incluido comentario
 class HelloWorld {
 	// Your program begins with a call to main().
 	// Prints "Hello, World" to the terminal window.
